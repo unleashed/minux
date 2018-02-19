d6e0f14860af1f33a5c9f344d3d034d0ce1fd1ff
Alejandro Martinez Ruiz <amr@redhat.com>
2018-02-19 01:19:30 +0000

    src/lib: show "Panic!" on VGA when panicking

--- src/lib.rs
+++ src/lib.rs
@@ -16,4 +16,12 @@ pub extern fn eh_personality() {}
 
 #[lang = "panic_fmt"]
 #[no_mangle]
-pub extern fn panic_fmt() -> ! { loop {} }
+pub extern fn panic_fmt() -> ! {
+    let buffer_ptr = (0xb8000) as *mut _;
+    let red = 0x4f;
+    unsafe {
+        *buffer_ptr = [b'P', red, b'a', red, b'n', red,
+                       b'i', red, b'c', red, b'!', red];
+    };
+    loop {}
+}
